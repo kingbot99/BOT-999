1c2493148442868a7f01e661ea327a40204c0e65
kingbot99 <116874271+kingbot99@users.noreply.github.com>
2022-11-04 15:01:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,8 +29,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
+global.vcardowner = ['+94705505104'] // owner number
+global.ownername = "BOT 999" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
@@ -47,13 +47,13 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
-global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.owner = ['+94705505104']
+global.ownertag = '+94705505014' //your tag number
+global.botname = 'BOT 999 - 𝘉𝘖𝘛' //ur bot name
+global.packname = "BOT 999 Sticker" // sticker packname
+global.author = "BOT 999" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
+global.footer = '[ ◉BOT 999 ]' //
 
 //database
 global.premium = ['+380944814219'] //ur premium numbers
